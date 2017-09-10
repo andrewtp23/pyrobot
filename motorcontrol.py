90e669a4903dd948cc74214dc03df0dcaf6b579c
andrewtp23 <25349004+andrewtp23@users.noreply.github.com>
2017-09-10 00:00:50 -0400

    Update motorcontrol.py

--- motorcontrol.py
+++ motorcontrol.py
@@ -45,12 +45,3 @@ def left_turn(time_frame):
     setPins(True, False, True, False)
     time.sleep(time_frame)
     gpio.cleanup()
-
-
-
-
-
-
-                                                                                                              [ Read 52 lines ]
-^G Get Help                            ^O WriteOut                            ^R Read File                           ^Y Prev Page                           ^K Cut Text                            ^C Cur Pos
-^X Exit                                ^J Justify                             ^W Where Is                            ^V Next Page                           ^U UnCut Text                          ^T To Spell
